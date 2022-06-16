9f9ea12add5308fad87e0fbbe1ed1b8a6879f182
Annie Cai <caishajia@gmail.com>
2022-06-16 18:50:40 -0400

    Add files via upload

---
+++ recommendations.js
@@ -0,0 +1,90 @@
+data = [
+    {
+        "name": "cica",
+        "link": "https://www.chatelaine.com/style/beauty/centella-asiatica-cica-skincare-ingredient",
+        "desc": "centella asiatica, or more commonly known as cica, is a plant herb that is known for its medicinal properties. it is rich in antioxidants and amino acids. cica is used to tackle blemishes and soothe, moisturize and heal the skin",
+        "rec": "dr.jart+ cicapair tiger grass cream",
+        "reclink": "https://www.drjart.com/product/28258/94008/moisturizers/cicapair-tiger-grass-cream?size=50_mL%2F1.69_fl._oz."
+    },
+
+    {
+        "name": "ceramide",
+        "link": "https://www.byrdie.com/ceramides-4693671",
+        "desc": "ceramides are lipids found in our skin. they help to moisturize and strengthen the protective skin barrier, and protect the skin from external aggressors such as weather and pollution.",
+        "rec": "dr. jart+ ceramidin cream",
+        "reclink": "https://www.drjart.com/product/28258/90156/moisturizers/ceramidin-cream?size=1.69_oz%2F50_ml"
+    },
+
+    {
+        "name": "glycolic acid",
+        "link": "https://www.verywellhealth.com/glycolic-acid-15774",
+        "desc": "glycolic acid is a water-soluble alpha hydroxy acid (aha) made from sugar cane. glycolic acid is an acne-fighting acid that helps remove dead skin cells and stimulate collagen production. because the molecules are tiny, it is easy for the skin to absorb. this makes them ideal for smoothing fine lines, improving skin texture, and other anti-aging uses",
+        "rec": "the ordinary glycolic acid 7% toning solution",
+        "reclink": "https://theordinary.com/en-ca/glycolic-acid-7-toning-solution-exfoliator-100418.html"
+    },
+
+    {
+        "name": "green tea",
+        "link": "https://www.byrdie.com/green-tea-for-skin-4843092",
+        "desc": "green tea is derived from the leaves and buds of the tea plant camellia sinensis. green tea has antioxidant and anti-inflammatory properties, helping to reduce sebum and soothe the skin",
+        "rec": "innisfree intensive hydrating serum with green tea seed",
+        "reclink": "https://us.innisfree.com/collections/green-tea/products/intensive-hydrating-serum"
+    },
+
+    {
+        "name": "hyaluronic acid",
+        "link": "https://www.byrdie.com/what-is-hyaluronic-acid",
+        "desc": "hyaluronic acid is a superstar at alleviating dry skin. it helps to replenish and hold cell moisture, leading to hydrated, plump skin",
+        "rec": "corsx hyaluronic acid hydra power essense",
+        "reclink": "https://www.cosrx.com/products/hyaluronic-acid-hydra-power-essence"
+    },
+
+    {
+        "name": "niacinamide",
+        "link": "https://www.byrdie.com/vitamin-b3-benefits",
+        "desc": "niacinamide is a form of vitamin b3 that is great for brightning and hydrating the skin. niacinamide is effective at clearing hyperpigmentation and evening out your skin tone. it helps to reduce redness and irritation and is great for those with sensitive skin",
+        "rec": "beauty of joseon glow serum: propolis + niacinamide",
+        "reclink": "https://beautyofjoseon.com/products/glow-serum-propolis-niacinamide"
+    },
+
+    {
+        "name": "rice",
+        "link": "https://www.healthshots.com/beauty/natural-cures/5-benefits-of-using-rice-water-for-skin/",
+        "desc": "rice is used in many skincare products in asia. it is loaded with amino acids and skin-healing nutrients to reduce acne, blemishes, and pores. rice also helps with cell regeneration and moisture retention",
+        "rec": "haru haru black rice hyaluronic toner",
+        "reclink": "https://haruharuusa.com/products/wonder-black-rice-hyaluronic-toner"
+    },
+
+    {
+        "name": "salicylic acid",
+        "link": "https://www.allure.com/story/what-does-salicylic-acid-do",
+        "desc": "salicylic acid is a type of beta hydroxy acid (bha) that acts as a chemical exfoliant. salicylic acid penetrates deep into the skin to dissolve skin debris that clogs pores. it is great for surface acne like whiteheads and blackheads",
+        "rec": "paula's choice 2% bha liquid exfoliant",
+        "reclink": "https://www.paulaschoice.com/skin-perfecting-2pct-bha-liquid-exfoliant/201-2010.html"
+    },
+
+    {
+        "name": "snail mucin",
+        "link": "https://www.byrdie.com/snail-mucin-for-skin-4768132",
+        "desc": "snail mucin is a cult favourite ingredient in korean skincare. it is packed with natural nutrients that help moisturize the skin. it also promotes collagen production and has anti-aging, brightening, and soothing properties",
+        "rec": "corsx advanced snail 96 mucin power essense",
+        "reclink": "https://www.cosrx.com/products/advanced-snail-96-mucin-power-essence"
+    },
+
+    {
+        "name": "sunscreen",
+        "link": "https://mahoneydermatology.com/5-important-reasons-to-wear-sunscreen-daily/",
+        "desc": "sunscreen, although not an ingredient, is a vital part of any skincare routine. sunscreen protects our skin from harmful uv rays. it prevents skin cancer, sunburns, and also boost your skin's hydration",
+        "rec": "beauty of joseon relief sun: rice + probiotics",
+        "reclink": "https://beautyofjoseon.com/products/relief-sun-rice-probiotics"
+    },
+
+    {
+        "name": "vitamin c",
+        "link": "https://www.byrdie.com/vitamin-c-benefits",
+        "desc": "vitamin c is an essential nutrient for the body. it is powerful antioxidant that protects the skin from uv exposure. it also brightens the skin and promotes collagen production",
+        "rec": "klairs freshly juiced vitamin drop",
+        "reclink": "https://www.klairscosmetics.com/products/freshly-juiced-vitamin-drop/"
+    },  
+
+]
